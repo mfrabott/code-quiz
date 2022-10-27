07a3cc7a6d70938a6bd605315530fa367477b008
mfrabott <mfrabott14@gmail.com>
2022-10-27 09:52:37 -0400

    break down challenge criteria and organize repository.

---
+++ assets/js/js.js
@@ -0,0 +1,19 @@
+
+// start button
+
+// timer
+
+// questions
+
+// answers
+
+// WHEN I answer a question
+// THEN I am presented with another question
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+// stats
