79e943b596b9a658770ee3e6542281404aa25bf7
mfrabott <mfrabott14@gmail.com>
2022-10-30 01:12:09 -0400

    Adjust styles and clean up comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+
 // DOM Accessed variables
 var startButton = document.querySelector(".start-button")
 var timerEl = document.querySelector(".countdown");
@@ -10,6 +11,7 @@ var answerFour = document.querySelector(".answer-d");
 var questionBlock = document.querySelector(".quiz-question")
 var userInputBlock = document.querySelector(".user-input")
 var highScoreBlock = document.querySelector(".high-scores")
+
 
 // Empty Variables
 var currentQuestion = "";
@@ -30,14 +32,13 @@ questionBank = [
     {questionText: "This is question 5", answers: ["Opt1", "Opt2", "Opt3", "Opt4"]}
 ];
 
-// TODO: VAR SCORE
 
-// FUNCTION: TIMER
+// FUNCTION: TIMER / Started on press of start button
 var countdown = function () {
     timeLeft = 10;
     var timeInterval = setInterval(function () {
       if (timeLeft >= 1) {
-        timerEl.textContent = ("Time remaining: \n" + timeLeft);
+        timerEl.textContent = ("Time remaining: \n" +  timeLeft);
         timeLeft--;
         return timeLeft;
       } else {
@@ -46,7 +47,8 @@ var countdown = function () {
         clearInterval(timeInterval);        
       };
     }, 1000);
-  };
+};
+
 
 // FUNCTION: START BUTTON
 var startQuiz = function(){
@@ -59,10 +61,12 @@ var startQuiz = function(){
     });
 };
 
+
 // Random number gererator
 function randomNumber(max) {
     return Math.floor(Math.random() * max);
 };
+
 
 // FUNCTION: RANDOMLY SELECT QUESTION FROM BANK
 var chooseQuestion = function(){
@@ -74,7 +78,9 @@ var chooseQuestion = function(){
         return correctAnswer;
 };
 
+
 // FUNCTION: RANDOMIZE ANSWER ORDER
+// TODO: MAKE INTO FOR LOOP
  var shuffleAnswers = function(){
     answerPullOrder = randomNumber(currentQuestion.answers.length);
     optionOne = currentQuestion.answers[answerPullOrder];
@@ -89,7 +95,8 @@ var chooseQuestion = function(){
     optionFour = currentQuestion.answers[answerPullOrder];
  };
 
-    // FUNCTION: DISPLAY QUESTION AND ANSWER
+
+// FUNCTION: DISPLAY QUESTION AND ANSWER
 var displayNewQuestion = function(){
     askedQuestion.textContent = currentQuestion.questionText;
     answerOne.textContent =  optionOne;
@@ -97,6 +104,7 @@ var displayNewQuestion = function(){
     answerThree.textContent = optionThree;
     answerFour.textContent = optionFour;
 };
+
 
 var newQuestion = function(){
     if (questionBank.length > 0) {
@@ -108,21 +116,21 @@ var newQuestion = function(){
     }
 };
 
+
 // TODO: FUNCTION: correctChoice
 var correctChoice = function() {
     console.log("correct");
+    // TODO: MESSAGE(CORRECT!)
+    // TODO: SCORE ++
 };
-
-// TODO: MESSAGE(CORRECT!)
-        // TODO: SCORE ++
 
 // TODO: FUNCTION: incorrectChoice
 var incorrectChoice = function() {
     console.log("incorrect")
+    // TODO: MESSAGE(THE CORRECT ANSWER WAS __)
+    // TODO: REDUCE TIME
 };
 
-    // TODO: MESSAGE(THE CORRECT ANSWER WAS __)
-        // TODO: REDUCE TIME
 
 // FUNCTION: USER MAKES SELECTION
 var userSelection = function(){
@@ -153,13 +161,9 @@ var userSelection = function(){
         if (optionThree === correctAnswer){
             correctChoice();
             newQuestion();
-            // console.log(currentQuestion);
-            // console.log(correctAnswer);
         } else {
             incorrectChoice();
             newQuestion();
-            // console.log(currentQuestion);
-            // console.log(correctAnswer);
         }
     });
 
@@ -168,13 +172,9 @@ var userSelection = function(){
         if (optionFour === correctAnswer){
             correctChoice();
             newQuestion();
-            // console.log(currentQuestion);
-            // console.log(correctAnswer);
         } else {
-            incorrectChoice();
+            incorrectChoice()
             newQuestion();
-            // console.log(currentQuestion);
-            // console.log(correctAnswer);
         }
     });
 };
@@ -189,7 +189,7 @@ var gameOver = function(){
 };
 
     
-    // TODO: ASK NAME TO ADD TO HIGH SCORES
+// TODO: ASK NAME TO ADD TO HIGH SCORES
        
     // SAVE LOCAL NAME AND SCORE
         
@@ -200,20 +200,13 @@ var gameOver = function(){
         // highScoreBlock.setAttribute("style", "display: contents");
 
 
-            
+// TODO: DISPLAY HIGH SCORES
+
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
 
 
-
 // TODO: stats
-
-
-
-// console.log(timeLeft);
-// Nest everything in while loop, while timer runs…load random question with answers, event.target the right answer
-
-
 
 
 startQuiz();
